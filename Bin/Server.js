2a3a87e62ffb6715fcf502ccb49e4ad4c0f26446
Steel Brain <aneesiqbalbhatti@gmail.com>
2015-05-16 17:31:02 -0700

    :new: Add Server Bin

---
+++ Bin/Server.js
@@ -0,0 +1,16 @@
+"use strict";
+var Cluster = require('cluster');
+var Port = Number(process.argv[2]);
+if(Cluster.isMaster){
+  if(Port !== Port){
+    console.error("Please specify a port to listen on!");
+    process.exit(1);
+  }
+  var NumCores = require('os').cpus().length;
+  for(var I = 0; I < NumCores; ++ I){
+    Cluster.fork();
+  }
+} else {
+  var Polyfill = require('../Main');
+  Polyfill.Listen(Port);
+}
