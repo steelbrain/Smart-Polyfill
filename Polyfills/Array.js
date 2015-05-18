6fa4a80ab05b81f3eae40daec764d8b8528415f8
Steel Brain <aneesiqbalbhatti@gmail.com>
2015-05-17 19:26:40 -0700

    :new: Add Array.isArray polyfill

---
+++ Polyfills/Array.js
@@ -0,0 +1,10 @@
+"use strict";
+// Polyfills for Array Functions
+SmartPolyfill.register('Array.isArray', {
+  Chrome: '1 - 4',
+  Firefox: '1 - 3',
+  IE: '1 - 8',
+  Safari: '1 - 4'
+}, function(o){
+  return Object.prototype.toString.call(o) === '[object Array]';
+});
