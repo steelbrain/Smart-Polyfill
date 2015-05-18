2f95623704383282c3d4d2f319efa8f893649a1c
Steel Brain <aneesiqbalbhatti@gmail.com>
2015-05-17 19:39:25 -0700

    :art: Add Opera field to polyfills

--- Polyfills/Date.js
+++ Polyfills/Date.js
@@ -4,7 +4,8 @@ SmartPolyfill.register('Date.now', {
   Chrome: '1 - 4',
   Firefox: '1 - 2',
   IE: '1 - 8',
-  Safari: '1 - 3'
+  Safari: '1 - 3',
+  Opera: '1 - 10'
 }, function(){
   return new Date().getTime();
 });
