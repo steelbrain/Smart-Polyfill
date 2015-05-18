4f739b9c38dc4af6f4ce0642b57c458c76faa336
Steel Brain <aneesiqbalbhatti@gmail.com>
2015-05-17 19:22:46 -0700

    :new: Add Date.prototype.toISOString polyfill

--- Polyfills/Date.js
+++ Polyfills/Date.js
@@ -8,3 +8,17 @@ SmartPolyfill.register('Date.now', {
 }, function(){
   return new Date().getTime();
 });
+SmartPolyfill.register('Date.prototype.toISOString', {
+  IE: '1 - 8'
+}, function(){
+  var a = this;
+  return (
+    1e3
+    -~a.getUTCMonth()
+    *10
+    +a.toUTCString()
+    +1e3+a/1
+  ).replace(
+    /1(..).*?(\d\d)\D+(\d+).(\S+).*(...)/,
+    '$3-$1-$2T$4.$5Z')
+});
