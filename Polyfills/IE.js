e66c50dc3ae0f3036f30b077b033c27eae5ebc21
Steel Brain <aneesiqbalbhatti@gmail.com>
2015-05-17 19:09:17 -0700

    :new: Add Object.keys polyfill

--- Polyfills/IE.js
+++ Polyfills/IE.js
@@ -4,18 +4,26 @@ SmartPolyfill.register('Object.create', {
   Chrome: '1 - 4',
   Firefox: '1 - 3',
   IE: '1 - 8',
-  Netscape: '*',
   Safari: '1 - 4'
 }, function(o, props){
   function F() {}
   F.prototype = o;
-
-  if (typeof(props) === "object") {
-    for (var prop in props) {
-      if (props.hasOwnProperty(prop)) {
+  if (typeof(props) === "object")
+    for (var prop in props)
+      if (props.hasOwnProperty(prop))
         F[prop] = props[prop];
-      }
-    }
-  }
   return new F();
+});
+SmartPolyfill.register('Object.keys', {
+  Chrome: '1 - 4',
+  Firefox: '1 - 3',
+  IE: '1 - 8',
+  Opera: '1 - 11',
+  Safari: '1 - 4'
+}, function(o) {
+  if (o !== Object(o))
+    throw new TypeError('Object.keys called on a non-object');
+  var r = [];
+  for (r[r.length] in o);
+  return r
 });
